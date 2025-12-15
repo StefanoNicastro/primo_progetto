941bd32ca40d439f9c39d9c454b0044f7fb5eefd
StefanoNicastro <stefano.nicastro@alessandrinimainardi.edu.it>
2025-12-15 13:43:27 +0100

    passo16_completo

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                  os.path.join(BASE_DIR, 'seconda_app/templates/seconda_app'),
                  os.path.join(BASE_DIR, 'templates'),
                  os.path.join(BASE_DIR, 'prova_pratica_1/templates/prova_pratica_1'),
+                 os.path.join(BASE_DIR, 'news/templates/news'),
                  os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
